b84c5020c69981ef34db5bccd9e4d7958c1381de
Valen <wang_wenl@worksap.co.jp>
2020-10-22 16:58:31 +0800

    add watcher for template and config

--- build/setup-dev-server.js
+++ build/setup-dev-server.js
@@ -1,17 +1,49 @@
+const path = require("path");
+const fs = require("fs");
+const webpack = require("webpack");
+const chokidar = require("chokidar");
+
+const resolve = (file) => path.resolve(__dirname, file);
+
 module.exports = (server, callback) => {
   let ready;
-  const onReady = new Promise(r => ready = r);
+  const onReady = new Promise((r) => (ready = r));
+
+  let serverBundle;
+  let template;
+  let clientManifest;
 
   // 监视打包构建, 调用callback生成renderer
-  const update = (serverBundle, template, clientManifest) => {
+  const update = () => {
     if (serverBundle && template && clientManifest) {
       callback(serverBundle, template, clientManifest);
       ready();
     }
-  }
+  };
 
   // watching serverBundle, template, clientManifest, and call update
-  
+
+  // fs.watch, fs.watchFile, not good enough, use chokidar
+  const templatePath = resolve("../index.template.html");
+  template = fs.readFileSync(templatePath, "utf-8");
+  chokidar.watch(templatePath).on("change", () => {
+    template = fs.readFileSync(templatePath, "utf-8");
+    console.log("template update", template);
+    update();
+  });
+
+  const serverConfig = require("./webpack.server.config");
+  const serverCompiler = webpack(serverConfig);
+  serverCompiler.watch({}, (err, stats) => {
+    if (err) throw err; // webpack err, like config err
+    if (stats.hasErrors()) return; // our code err
+    serverBundle = JSON.parse(
+      fs.readFileSync(resolve("../dist/vue-ssr-server-bundle.json"), "utf-8")
+    ); // not use require, for it has cache for old content
+    console.log("serverBundle update", serverBundle);
+
+    update();
+  });
 
   return onReady;
-}
+};
