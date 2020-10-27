8a317540dc22e38595dc224b6dc880c229c419ba
Valen <wang_wenl@worksap.co.jp>
2020-10-27 15:35:30 +0800

    add webpack-hot-middleware to hot reload

--- build/setup-dev-server.js
+++ build/setup-dev-server.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const webpack = require("webpack");
 const chokidar = require("chokidar");
 const devMiddleware = require("webpack-dev-middleware");
+const hotMiddleware = require("webpack-hot-middleware");
 
 const resolve = (file) => path.resolve(__dirname, file);
 
@@ -48,14 +49,23 @@ module.exports = (server, callback) => {
   });
 
   const clientConfig = require("./webpack.client.config");
+  clientConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  clientConfig.entry.app = [
+    // 和服务端交互处理热更新的客户端脚本
+    "webpack-hot-middleware/client",
+    clientConfig.entry.app,
+  ];
+  // 在热更新模式下确保每次编译生成的文件名字一致
+  clientConfig.output.filename = "[name].js";
+
   const clientCompiler = webpack(clientConfig);
-  const clientMiddleware = devMiddleware(clientCompiler, {
+  const clientDevMiddleware = devMiddleware(clientCompiler, {
     publicPath: clientConfig.output.publicPath,
     logLevel: "silent",
   });
   clientCompiler.hooks.done.tap("update client", () => {
     clientManifest = JSON.parse(
-      clientMiddleware.fileSystem.readFileSync(
+      clientDevMiddleware.fileSystem.readFileSync(
         resolve("../dist/vue-ssr-client-manifest.json", "utf-8")
       )
     );
@@ -64,7 +74,13 @@ module.exports = (server, callback) => {
   });
 
   // 将clientMiddleware挂载到express服务中, 提供对其托管的内存数据访问
-  server.use(clientMiddleware)
+  server.use(clientDevMiddleware);
+  server.use(
+    hotMiddleware(clientCompiler, {
+      noInfo: true,
+      publicPath: clientConfig.output.publicPath,
+    })
+  );
 
   return onReady;
 };
