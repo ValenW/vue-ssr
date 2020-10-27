b79ec4be0b2d155e75b942c3b96493bf53c3e5ef
Valen <wang_wenl@worksap.co.jp>
2020-10-27 19:05:00 +0800

    init Posts page

---
+++ src/pages/Posts.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "Posts",
+  data() {
+    return {};
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
