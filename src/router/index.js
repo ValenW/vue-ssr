2c82bf106b65d23647056d688c9679384367afd6
Valen <wang_wenl@worksap.co.jp>
2020-10-27 17:20:09 +0800

    add router and inject in app

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/pages/Home";
+
+Vue.use(VueRouter);
+
+export const createRouter = () => {
+  const router = new VueRouter({
+    // 使用history模式兼容前后端, 绝大多数服务端不会处理hash
+    mode: "history",
+    routes: [
+      {
+        path: "/",
+        name: "home",
+        component: Home,
+      },
+      {
+        path: "/about",
+        name: "about",
+        component: () => import("@/pages/About"), // 动态懒加载
+      },
+      {
+        path: "*",
+        name: "404",
+        component: () => import("@/pages/404"),
+      },
+    ],
+  });
+};
