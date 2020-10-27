50d178b64e91b68477c15af3750a03d596c8ecf2
Valen <wang_wenl@worksap.co.jp>
2020-10-27 19:03:13 +0800

    config vuex and export in app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueMeta from "vue-meta";
 import { createRouter } from "./router";
+import { createStore } from "./store";
 
 Vue.use(VueMeta);
 
@@ -15,10 +16,12 @@ Vue.mixin({
 // 应用程序、router 和 store 实例
 export function createApp() {
   const router = createRouter();
+  const store = createStore();
   const app = new Vue({
     // 根实例简单的渲染应用程序组件。
     render: (h) => h(App),
+    store,
     router,
   });
-  return { app, router };
+  return { app, router, store };
 }
---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+
+Vue.use(Vuex);
+
+export const createStore = () => {
+  return new Vuex.Store({
+    state: () => {
+      posts: [];
+    },
+    mutations: {
+      setPosts(state, data) {
+        state.posts = data;
+      },
+    },
+
+    actions: {
+      // 在服务端渲染期间必须让action返回Promise
+      async getPosts({ commit }) {
+        const data = await axios.get("https://cnnodejs.org/api/v1/topics");
+        commit("setPosts", data.data);
+      },
+    },
+  });
+};
